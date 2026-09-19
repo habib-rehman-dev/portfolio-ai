c3525cd00791aede8a00ffe3592d34c54dd85fe1
habib-rehman-dev <contact.habib.dev@gmail.com>
2026-09-19 22:25:48 +0500

    Fix CORS middleware URL by removing trailing slash

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000" , "https://habib-dev-605.vercel.app/"],
+    allow_origins=["http://localhost:3000" , "https://habib-dev-605.vercel.app"],
     allow_methods=["POST"],
     allow_headers=["*"],
 )
